01da785c3175adb2e38878eea5d966657c9dadf7
rlawngid0624 <109637767+rlawngid0624@users.noreply.github.com>
2022-07-20 12:18:33 +0900

    Add files via upload

--- app.py
+++ app.py
@@ -1,13 +1,33 @@
 import streamlit as st
+import sqlite3
 
-st.subheader('2. radio button test')
-food = st.radio(
-     "좋아하는 음식은 무엇인가요?",
-     ('초밥', '짜장면', '김치볶음밥'))
+con = sqlite3.connect('db.db')
+cur = con.cursor()
 
-if food == '초밥':
-    st.write('You selected 초밥.')
-elif food == '짜장면':
-    st.write('You selected 짜장면.')
-elif food == '김치볶음밥':
-    st.write('You selected 김치볶음밥')
+def login_user(id, pw):
+    cur.execute(f"SELECT * FROM users WHERE id = '{id}' and pwd = '{pw}'")
+
+    return cur.fetchone()
+
+menu = st.sidebar.selectbox('MENU', options=['로그인','회원가입','회원목록'])
+
+if menu == '로그인':
+    st.subheader('로그인')
+
+    login_id = st.text_input('아이디', placeholder='아이디를 입력하세요')
+    login_pw = st.text_input('비밀번호', placeholder='비밀번호를 입력하세요',type='password')
+
+    login_btn = st.button('로그인')
+    if login_btn:
+        user_info = login_user(login_id,login_pw)
+        st.write(user_info[0])
+        st.write(user_info[1])
+        st.write(user_info[2])
+        st.write(user_info[4],'님, 환영합니다')
+    st.sidebar.title('로그인')
+if menu == '회원가입':
+    st.subheader('회원가입')
+    st.sidebar.title('회원가입')
+if menu == '회원목록':
+    st.subheader('회원목록')
+    st.sidebar.title('회원목록')
